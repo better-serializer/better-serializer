4b0b41c8a34ccb1c1134b77a748c5fbdefd4e20f
mfris <rasta@lj.sk>
2017-02-07 00:33:45 +0100

    test

--- src/BetterSerializer/DataBind/MetaData/Reader.php
+++ src/BetterSerializer/DataBind/MetaData/Reader.php
@@ -4,6 +4,8 @@
  */
 namespace BetterSerializer\DataBind\MetaData;
 
+use phpDocumentor\Reflection\DocBlockFactoryInterface;
+
 /**
  * Class Reader
  * @author mfris
@@ -12,4 +14,26 @@ namespace BetterSerializer\DataBind\MetaData;
 final class Reader
 {
 
+    /**
+     * @var DocBlockFactoryInterface
+     */
+    private $docBlockFactory;
+
+    /**
+     * Reader constructor.
+     * @param DocBlockFactoryInterface $docBlockFactory
+     */
+    public function __construct(DocBlockFactoryInterface $docBlockFactory)
+    {
+        $this->docBlockFactory = $docBlockFactory;
+    }
+
+    /**
+     * @param string $className
+     * @return MetaData
+     */
+    public function read(string $className): MetaData
+    {
+        return new MetaData();
+    }
 }
